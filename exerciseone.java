30b9dada404ea51e510e6d6aadd3ab06ab73c009
NaomiUwase142 <149668498+NaomiUwase@users.noreply.github.com>
2023-11-03 15:02:48 +0200

    Add files via upload
    if else

---
+++ exerciseone.java
@@ -0,0 +1,14 @@
+public class exerciseone {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 15;
+        if (b > a) {
+            System.out.println("A is greater than B");
+        } else {
+            System.out.println("B is greater than A");
+        }
+    }
+}
+
+
+
