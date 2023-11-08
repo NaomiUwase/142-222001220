e3b2cb12a5179b81396160ac0ab91a70cfc8a6e5
NaomiUwase142 <149668498+NaomiUwase@users.noreply.github.com>
2023-11-08 20:54:52 +0200

    App.java

--- App.java
+++ App.java
@@ -2,56 +2,79 @@ interface Activity {
     void describeActivity();
 }
 
-class ActivityBase implements Activity {
+class IndoorActivity implements Activity {
     private String name;
+    private int duration;
 
-    public ActivityBase(String name) {
+    public IndoorActivity(String name, int duration) {
         this.name = name;
+        this.duration = duration;
     }
 
     public void describeActivity() {
-        System.out.println("Activity: " + name);
+        System.out.println("Indoor Activity: " + name);
+        System.out.println("Duration (in minutes): " + duration);
     }
 }
 
-class IndoorActivity extends ActivityBase {
-    private int participants;
+class OutdoorActivity implements Activity {
+    private String name;
+    private int duration;
 
-    public IndoorActivity(String name, int participants) {
-        super(name);
-        this.participants = participants;
+    public OutdoorActivity(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public void describeActivity() {
+        System.out.println("Outdoor Activity: " + name);
+        System.out.println("Duration (in minutes): " + duration);
+    }
+}
+
+class Yoga extends IndoorActivity {
+    private String style;
+
+    public Yoga(String name, int duration, String style) {
+        super(name, duration);
+        this.style = style;
     }
 
     @Override
     public void describeActivity() {
         super.describeActivity();
-        System.out.println("Type: Indoor");
-        System.out.println("Number of Participants: " + participants);
+        System.out.println("Yoga Style: " + style);
     }
 }
 
-class OutdoorActivity extends ActivityBase {
+class Hiking extends OutdoorActivity {
     private String location;
 
-    public OutdoorActivity(String name, String location) {
-        super(name);
+    public Hiking(String name, int duration, String location) {
+        super(name, duration);
         this.location = location;
     }
 
     @Override
     public void describeActivity() {
         super.describeActivity();
-        System.out.println("Type: Outdoor");
-        System.out.println("Location: " + location);
+        System.out.println("Hiking Location: " + location);
     }
 }
 
-class ActivityDemo {
+public class ActivityDemo {
     public static void main(String[] args) {
-        Activity indoorActivity = new IndoorActivity("Board Games", 4);
-        Activity outdoorActivity = new OutdoorActivity("Hiking", "Mountain Trail");
+        Activity indoorActivity = new IndoorActivity("Reading", 60);
+        Activity outdoorActivity = new OutdoorActivity("Running", 45);
+        Activity yoga = new Yoga("Yoga", 90, "Vinyasa");
+        Activity hiking = new Hiking("Hiking", 120, "Mountain Trails");
 
         indoorActivity.describeActivity();
+        System.out.println();
         outdoorActivity.describeActivity();
+        System.out.println();
+        yoga.describeActivity();
+        System.out.println();
+        hiking.describeActivity();
     }
 }
